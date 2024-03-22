4aaef9b3ba7dca8adf6da38ba0788f9f9bb23f91
Luiz <105563531+Luiz-Fernando-Policarpo-Leandro@users.noreply.github.com>
2024-03-22 18:58:20 -0300

    add team

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
     @item = [["Do you want to create an e-commerce?", "Count on us, we have more than 30 stores in our portfolio.","#","project e-commerce","See portifolio"], 
       ["Are you looking to get the project off the ground?","Our software engineering team is ready to assist you.","#","Software Engineering", "Contact us"] , 
       ["Are you looking for maintenance on your software?","hDC has highly qualified engineers to solve your problem.","#","Software maintenance","Contact us"]]
+    @team = [['Jhon Doe','Software Developer'],['Maria Mariana','SEO Consultant'],['Paul Baker','Project Manager'],['Karina Katarina','UX/UI Designer']]
   end
 end
